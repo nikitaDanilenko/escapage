c26cc53f6eb07b57620931882852271f0188dee2
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-05-31 18:44:52 +0200

    Add module for colors

---
+++ src/Elements/Colors.elm
@@ -0,0 +1,17 @@
+module Elements.Colors exposing (..)
+
+
+type alias RGBA =
+    { red : Float
+    , green : Float
+    , blue : Float
+    , alpha : Float
+    }
+
+
+type alias Navigation =
+    { reload : RGBA
+    , back : RGBA
+    }
+
+
