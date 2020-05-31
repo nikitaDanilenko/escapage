f2defb1fa3d360de9ec213fb6830c2613faf19b9
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-05-31 18:21:15 +0200

    Start work on visualising inventories

--- src/Elements/Inventory.elm
+++ src/Elements/Inventory.elm
@@ -1,14 +1,45 @@
 module Elements.Inventory exposing (..)
 
 import Elements.Unlockable exposing (Unlockable, unlockable)
-
+import Html exposing (Html, button, div, label, text)
+import Html.Attributes exposing (id)
+import Ionicon
 
 type alias Inventory =
-    { darkMode : Unlockable String
+    { darkMode : DarkMode
     }
 
+type DarkMode = DarkMode (Unlockable String)
 
 empty : Inventory
 empty =
-    { darkMode = unlockable "darkMode"
+    { darkMode = DarkMode (unlockable "darkMode")
     }
+
+type alias DarkModeFunctions model msg = {
+    activate : model -> Cmd msg,
+    deactivate : model -> Cmd msg
+    }
+
+type alias Functions model msg = {
+    darkModeFunction : DarkModeFunctions model msg
+    }
+
+type alias RGBA =
+    { red : Float
+    , green : Float
+    , blue : Float
+    , alpha : Float
+    }
+
+
+type alias Icon msg =
+    Int -> RGBA -> Html msg
+
+
+
+view : Inventory -> Html msg
+view inventory =
+    div [ id "inventory" ]
+        [ button [] [text "click?"],
+          label [] [Ionicon.alert 80 {red = 1, green = 0, blue = 1, alpha = 1}]]
