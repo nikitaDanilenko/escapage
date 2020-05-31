0e55845d6ec0bbdc3a684e867f724268662e8ff7
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-05-31 18:45:57 +0200

    Adjust import

--- src/Elements/Inventory.elm
+++ src/Elements/Inventory.elm
@@ -1,35 +1,35 @@
 module Elements.Inventory exposing (..)
 
+import Elements.Colors exposing (RGBA)
 import Elements.Unlockable exposing (Unlockable, unlockable)
 import Html exposing (Html, button, div, label, text)
 import Html.Attributes exposing (id)
 import Ionicon
 
+
 type alias Inventory =
     { darkMode : DarkMode
     }
 
-type DarkMode = DarkMode (Unlockable String)
+
+type DarkMode
+    = DarkMode (Unlockable String)
+
 
 empty : Inventory
 empty =
     { darkMode = DarkMode (unlockable "darkMode")
     }
 
-type alias DarkModeFunctions model msg = {
-    activate : model -> Cmd msg,
-    deactivate : model -> Cmd msg
+
+type alias DarkModeFunctions model msg =
+    { activate : model -> Cmd msg
+    , deactivate : model -> Cmd msg
     }
 
-type alias Functions model msg = {
-    darkModeFunction : DarkModeFunctions model msg
-    }
 
-type alias RGBA =
-    { red : Float
-    , green : Float
-    , blue : Float
-    , alpha : Float
+type alias Functions model msg =
+    { darkModeFunction : DarkModeFunctions model msg
     }
 
 
@@ -37,9 +37,9 @@ type alias Icon msg =
     Int -> RGBA -> Html msg
 
 
-
 view : Inventory -> Html msg
 view inventory =
     div [ id "inventory" ]
-        [ button [] [text "click?"],
-          label [] [Ionicon.alert 80 {red = 1, green = 0, blue = 1, alpha = 1}]]
+        [ button [] [ text "click?" ]
+        , label [] [ Ionicon.alert 80 { red = 1, green = 0, blue = 1, alpha = 1 } ]
+        ]
