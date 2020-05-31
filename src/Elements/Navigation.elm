369ffda06821f75ff5e4e929bf6c864f336b09f5
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-05-31 19:32:21 +0200

    Use size parameter and swap order of operations

--- src/Elements/Navigation.elm
+++ src/Elements/Navigation.elm
@@ -13,9 +13,9 @@ type alias Wiring msg =
     }
 
 
-navigation : Wiring msg -> Colors.Navigation -> Html msg
-navigation wiring colors =
+navigation : Wiring msg -> Colors.Navigation -> Int -> Html msg
+navigation wiring colors size =
     div [ id "navigation" ]
-        [ label [ onClick wiring.reload ] [ Ionicon.refresh 10 colors.reload ]
-        , label [ onClick wiring.back ] [ Ionicon.arrowLeftA 10 colors.back ]
+        [ label [ onClick wiring.back ] [ Ionicon.arrowLeftA size colors.back ]
+        , label [ onClick wiring.reload ] [ Ionicon.refresh size colors.reload ]
         ]
